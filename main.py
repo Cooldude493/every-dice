9b7dcb2b11cd6f8e33ca70da693c35eeb250f78b
Cooldude493 <jasongebanjr@gmail.com>
2025-12-09 10:50:47 -0500

    Products / Browse added

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 from flask import Flask, render_template
 
+import pymysql
+
+from dynaconf import Dynaconf
+
 app = Flask(__name__)
+
+config = Dynaconf(settings_file=["setting.toml"])
+
+def connect_db():
+    conn = pymysql.connect(
+    host="db.steamcenter.tech", 
+    user="jgeban",
+    password=config.password,
+    database="jgeban_every_dice",
+    autocommit=True,
+    cursorclass=pymysql.cursors.DictCursor
+    )
+
+    return conn
 
 @app.route('/')
 def index():
     return render_template("homepage.html.jinja")
 
+@app.route('/browse')
+def browse():
+    connection = connect_db()
+
+    cursor = connection.cursor()
+ 
+    cursor.execute("SELECT * FROM `Product`")
+    
+    result = cursor.fetchall()
+    
+    connection.close()
+    return render_template("browse.html.jinja")
